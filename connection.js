df4ec129f452eace884bce9cc5eccd0419214b3c
Lukas <leandrolukas916@gmail.com>
2023-10-16 20:05:43 -0300

    alter database name

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'lukas1234',
-  database : 'funcionarios'
+  database : 'istorm'
 });
 
 db.connect((err) => {
